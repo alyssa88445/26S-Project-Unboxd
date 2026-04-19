7e3e90bcd0847a94b2e07ff9aed418db85c2581e
Esha Kanakapura <kanakapura.e@northeastern.edu>
2026-04-19 15:28:36 -0400

    Added user stories to buyer home

--- app/src/pages/20_Buyer_Home.py
+++ app/src/pages/20_Buyer_Home.py
@@ -10,3 +10,18 @@ SideBarLinks()
 
 st.title(f"Welcome Buyer, {st.session_state['first_name']}.")
 st.write('### What would you like to do today?')
+
+if st.button('Filter Listings',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/21_Filter_Listings.py')
+
+if st.button('View Listing Information',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/22_View_Listing_Info.py')
+
+if st.button('View Seller Verification',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/23_View_Seller_Verification.py')
